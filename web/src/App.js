9113acb20e58a1633f947b97a95f6ce0923810de
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2023-06-09 09:57:41 -0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -12,7 +12,7 @@ const theme = createTheme(beautifyTheme)
 
 function MainComponent() {
   // temos que fazer autenticação
-  var signed = true;
+  let signed = true;
 
   if (!signed) return <PublicRoutes />
 
